c23622a51fd7d78b55e6eed67c0501f045160220
Nikola Kidman <1602211@mail.ru>
2023-09-12 21:07:57 +0600

    13 закончен

--- src/routes.php
+++ src/routes.php
@@ -3,7 +3,6 @@
 
 return [
     '~^hello/(.*)$~' => [\MyProject\Controllers\MainController::class, 'sayHello'],
-    '~^translate/$~' => [\MyProject\Controllers\TranslatorController::class, 'translate'],
     '~^article/$~' => [\MyProject\Controllers\MainController::class, 'newPage'],
     '~^$~' => [\MyProject\Controllers\MainController::class, 'main']
 ];
